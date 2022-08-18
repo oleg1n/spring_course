4705d2b38c95e7964b46daac569506cc78e406fa
Kuprijanov Oleg <oleg1n@mail.ru>
2022-08-18 13:53:59 +0300

    commit 2

---
+++ src/main/java/spring_introduction/Person.java
@@ -0,0 +1,45 @@
+package spring_introduction;
+
+public class Person {
+    private Pet pet;
+    private String surname;
+    private int age;
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void callYourPet(){
+        System.out.println("Common my pet!");
+        pet.say();
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "pet=" + pet +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
